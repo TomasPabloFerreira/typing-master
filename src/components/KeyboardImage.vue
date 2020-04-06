439d8217e34a5de65feb4e537f663a635bd5490d
TomasPabloFerreira <tomasferreira373@gmail.com>
2020-04-06 18:14:14 -0300

    feat(bootstrap): Bootstrap + KeyboardImage component

---
+++ src/components/KeyboardImage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="keyboard-image container-fluid">
+    <div class="row">
+      <div class="col-sm-3"></div>
+      <div class="col-sm-6">
+        <img id="keyboard-img" src="../../public/images/keyboard.png" />
+      </div>
+      <div class="col-sm-3"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "KeyboardImage",
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#keyboard-img {
+  width: 100%;
+}
+</style>
